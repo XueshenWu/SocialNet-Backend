31e6bb020384e2dd792e4866f343e493afdab1f5
Kent YAN <41227894+golden-ice@users.noreply.github.com>
2024-02-08 19:25:40 -0500

    tried the db imports

--- src/byan/profile/profile.module.ts
+++ src/byan/profile/profile.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { XueshenModule } from 'src/xueshen/xueshen.module';
+import { DbService } from 'src/xueshen/db/db.service';
+import { DbModule } from 'src/xueshen/db/db.module';
 
 @Module({
+  imports: [DbModule],
+  providers: [DbService, ProfileService],
   controllers: [ProfileController],
-  providers: [ProfileService],
 })
 export class ProfileModule {}
--- src/xueshen/db/db.module.ts
+++ src/xueshen/db/db.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { DbService } from './db.service';
 import { ConnectionService } from './connection.service';
 import { DbPostService } from './db_post.service';
+import { DbUserService } from './db_user.service';
+
 @Module({
-  providers: [DbService, ConnectionService, DbPostService],
+  providers: [DbService, ConnectionService, DbPostService, DbUserService],
   exports: [DbService]
 })
 export class DbModule {
