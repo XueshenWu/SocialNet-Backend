095759c84043aa123fe9b18e866739c5bcdff952
Kent YAN <41227894+golden-ice@users.noreply.github.com>
2024-02-15 23:25:39 -0500

    added unit testing for updateProfile

--- src/byan/profile/profile.service.spec.ts
+++ src/byan/profile/profile.service.spec.ts
@@ -1,19 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfileService } from './profile.service';
 import { DbService } from 'src/xueshen/db/db.service';
+import UpdateProfileDto from 'src/xueshen/dto/updateProfileDto';
 
 describe('ProfileService', () => {
   let service: ProfileService;
 
   beforeEach(async () => {
+    // Create a fake copy of the DbService
+    const fakeDbService: Partial<DbService> = {
+      updateProfile: (updateProfileDto: UpdateProfileDto) => 
+        Promise.resolve(true),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfileService],
+      providers: [
+        ProfileService,
+        {
+          provide: DbService,
+          useValue: fakeDbService,
+        },
+      ],
     }).compile();
 
     service = module.get<ProfileService>(ProfileService);
   });
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('update the profile for the given user', async () => {
+    const userProfileUpdated = await service.updateProfile({userId: "123", username: "John"} as UpdateProfileDto);
+    expect(userProfileUpdated).toEqual(true);
   });
 });
