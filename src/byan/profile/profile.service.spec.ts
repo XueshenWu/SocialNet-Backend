2d160ec53d930bdd5502c2ff3590c8b3834995f2
Kent YAN <41227894+golden-ice@users.noreply.github.com>
2024-02-18 17:32:39 -0500

    added unit testing for profile service

--- src/byan/profile/profile.service.spec.ts
+++ src/byan/profile/profile.service.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfileService } from './profile.service';
 import { DbUserService } from '../../xueshen/db/user/db_user.service';
+import { Profile } from "@prisma/pg";
 import UpdateProfileDto from 'src/xueshen/dto/updateProfileDto';
 
 describe('ProfileService', () => {
   let service: ProfileService;
+  let fakeDbUserService: Partial<DbUserService>;
 
   beforeEach(async () => {
     // Create a fake copy of the DbUserService
-    const fakeDbService: Partial<DbUserService> = {
-      query_profile_by_user_id: (userId: string) => 
-        Promise.resolve(null),
-      updateProfile: (updateProfileDto: UpdateProfileDto) => 
-        Promise.resolve(true),
+    const userProfiles: Profile[] = [];
+
+    const profile1 = {
+      userId : "1a",
+      username: "Amy",
+      fullname: 'Amy Green',
+      avatar: '1.jpg',
+      bio: 'power girl'
+    } as Profile;
+
+    const profile2 = {
+      userId : "2b",
+      username: "Bobby",
+      fullname: 'Bobby White',
+      avatar: '2.jpg',
+      bio: 'super man'
+    } as Profile;
+
+    const profile3 = {
+      userId : "3c",
+      username: "Cindy",
+      fullname: 'Cindy Black',
+      avatar: '3.jpg',
+      bio: 'shero'
+    } as Profile;
+
+    userProfiles.push(profile1);
+    userProfiles.push(profile2);
+    userProfiles.push(profile3);
+
+    fakeDbUserService = {
+      query_profile_by_user_id: (userId: string) =>  {
+        const filteredUserProfile = userProfiles.filter((profile) => profile.userId === userId);
+        if (!filteredUserProfile) {
+          return Promise.resolve(null);
+        } else {
+          return Promise.resolve(filteredUserProfile[0]);
+        }
+      },
+      updateProfile: (updateProfileDto: UpdateProfileDto) =>  {
+        return Promise.resolve(false)
+      }
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -20,8 +59,9 @@ describe('ProfileService', () => {
         ProfileService,
         {
           provide: DbUserService,
-          useValue: fakeDbService,
+          useValue: fakeDbUserService,
         },
+        
       ],
     }).compile();
 
@@ -32,8 +72,78 @@ describe('ProfileService', () => {
     expect(service).toBeDefined();
   });
 
+  it('return corresponding profile if an existing userId given', async () => {
+    const profile = await service.getProfileByUserId('1a');
+    expect(service).toBeDefined();
+    expect(profile.userId).toEqual('1a');
+  });
+
+  it('return undefined if an non-existing userId given', async () => {
+    const profile = await service.getProfileByUserId('ww');
+    expect(service).toBeDefined();
+    expect(profile).toEqual(undefined);
+  });
+
   it('update the profile for the given user', async () => {
-    //const userProfileUpdated = await service.updateProfile({userId: "123", username: "John"} as UpdateProfileDto);
-    //expect(userProfileUpdated).toEqual(true);
+    const userProfilesNew: Profile[] = [];
+
+    const profile1New = {
+      userId : "1a",
+      username: "Amy",
+      fullname: 'Amy Green',
+      avatar: '1.jpg',
+      bio: 'power girl'
+    } as Profile;
+
+    const profile2New = {
+      userId : "2b",
+      username: "Bobby",
+      fullname: 'Bobby White',
+      avatar: '2.jpg',
+      bio: 'super man'
+    } as Profile;
+
+    const profile3New = {
+      userId : "3c",
+      username: "Cindy",
+      fullname: 'Cindy Black',
+      avatar: '3.jpg',
+      bio: 'shero'
+    } as Profile;
+
+    userProfilesNew.push(profile1New);
+    userProfilesNew.push(profile2New);
+    userProfilesNew.push(profile3New);
+
+    userProfilesNew[2].avatar = '3u.jpg';
+    console.log(userProfilesNew[2].avatar);
+
+    fakeDbUserService.updateProfile = (updateProfileDto: UpdateProfileDto) =>  {
+        // const profile3Updated = {
+        //   userId : "3c",
+        //   username: "Cindy",
+        //   fullname: 'Cindy Black',
+        //   avatar: '3updated.jpg',
+        //   bio: 'shero Updated'
+        // } as Profile;
+        userProfilesNew[2].avatar = '3updated.jpg';
+        userProfilesNew[2].bio = 'shero Updated';
+        return Promise.resolve(false)
+      };
+
+    const userProfileUpdated = await service.updateProfile({
+      userId: '3c',
+      fullname: 'Cindy Black',
+      avatar: '3updated.jpg',
+      bio: 'shero Updated',
+      interests: [],
+      tags: [],
+      gender: '',
+      getNoneEmptyData: () => {
+        throw new Error('Function not implemented.');
+      }} as UpdateProfileDto);
+    expect(userProfilesNew[2].avatar).toEqual('3updated.jpg');
+    expect(userProfilesNew[2].bio).toEqual('shero Updated');
+    expect(userProfileUpdated).toEqual(false);
   });
 });
