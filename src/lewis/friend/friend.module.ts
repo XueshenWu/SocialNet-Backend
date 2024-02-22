62d53003a7f5c4937b8e03cc069d22783cc99294
Lewis <lewisning@yahoo.com>
2024-02-21 23:53:01 -0500

    Update friend.module.ts

--- src/lewis/friend/friend.module.ts
+++ src/lewis/friend/friend.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FriendService } from './friend.service';
 import { FriendController } from './friend.controller';
+import { DbUserService } from '../../xueshen/db/user/db_user.service';
+import { ConnectionService } from '../../xueshen/db/connection/connection.service';
 
 @Module({
   providers: [FriendService],
-  controllers: [FriendController]
+  controllers: [FriendController, DbUserService, ConnectionService]
 })
 export class FriendModule {}
