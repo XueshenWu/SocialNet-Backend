942d8950daa7e74e2076ae3cd3128175152e4b73
Xueshen Wu(x49wu) <x49wu@uwaterloo.ca>
2024-03-25 13:38:00 -0400

    change back listen adress to localhost, fixed redis module

--- src/xueshen/redis/redis.module.ts
+++ src/xueshen/redis/redis.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { DbPostService } from '../db/post/db_post.service';
+import { ConnectionService } from '../db/connection/connection.service';
 
 @Module({
-  providers: [RedisService]
+  
+  providers: [RedisService, DbPostService, ConnectionService],
+  exports:[RedisService]
 })
 export class RedisModule {
     
